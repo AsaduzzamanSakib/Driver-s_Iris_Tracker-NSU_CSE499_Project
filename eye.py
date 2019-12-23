d715e1f5dd1f60f0961a9cea790b77d785eaafad
Md Asaduzzaman Sakib <57108671+AsaduzzamanSakib@users.noreply.github.com>
2019-12-23 23:29:58 +0500

    Eye

---
+++ eye.py
@@ -0,0 +1,36 @@
+import math
+import numpy as np
+import cv2
+from .pupil import Pupil
+
+
+class Eye(object):
+    """
+    This class creates a new frame to isolate the eye and
+    initiates the pupil detection.
+    """
+
+    LEFT_EYE_POINTS = [36, 37, 38, 39, 40, 41]
+    RIGHT_EYE_POINTS = [42, 43, 44, 45, 46, 47]
+
+    def __init__(self, original_frame, landmarks, side, calibration):
+        self.frame = None
+        self.origin = None
+        self.center = None
+        self.pupil = None
+
+        self._analyze(original_frame, landmarks, side, calibration)
+
+    @staticmethod
+    def _middle_point(p1, p2):
+        """Returns the middle point (x,y) between two points
+
+        Arguments:
+            p1 (dlib.point): First point
+            p2 (dlib.point): Second point
+        """
+        x = int((p1.x + p2.x) / 2)
+        y = int((p1.y + p2.y) / 2)
+        return (x, y)
+
+    
