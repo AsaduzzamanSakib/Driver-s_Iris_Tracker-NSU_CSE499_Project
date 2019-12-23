c008f0a5e38c02140fbb32b10d6dcbd00f5b9bdc
MITU RANI DAS <47188195+MITU-RANI-DAS@users.noreply.github.com>
2019-12-23 10:24:33 -0800

    Calibration

---
+++ Calibration.py
@@ -0,0 +1,33 @@
+from __future__ import division
+import cv2
+from .pupil import Pupil
+
+
+class Calibration(object):
+    
+
+    def __init__(self):
+        self.nb_frames = 20
+        self.thresholds_left = []
+        self.thresholds_right = []
+
+    def is_complete(self):
+        
+        return len(self.thresholds_left) >= self.nb_frames and len(self.thresholds_right) >= self.nb_frames
+
+    def threshold(self, side):
+        
+        if side == 0:
+            return int(sum(self.thresholds_left) / len(self.thresholds_left))
+        elif side == 1:
+            return int(sum(self.thresholds_right) / len(self.thresholds_right))
+
+    @staticmethod
+    def iris_size(frame):
+        
+        frame = frame[5:-5, 5:-5]
+        height, width = frame.shape[:2]
+        nb_pixels = height * width
+        nb_blacks = nb_pixels - cv2.countNonZero(frame)
+        return nb_blacks / nb_pixels
+
